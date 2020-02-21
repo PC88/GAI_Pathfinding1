5a3fb6ff02f7850bfbae79ac4e082062866bd990
PC88 <32823980+PC88@users.noreply.github.com>
2020-02-21 05:07:27 +0000

    heuristic refactored to its own .h

--- PathFinding1/Entrypoint.cpp
+++ PathFinding1/Entrypoint.cpp
@@ -19,7 +19,6 @@
 #include <list>
 #include <iostream>
 #include <fstream>
-#include <math.h>    // for sqrt
 
 // abstraction of base code
 #include "Abstraction/Heuristic.h"
@@ -41,25 +40,6 @@ typedef float cost;
 
 
 
-
-// euclidean distance heuristic
-template <class Graph, class CostType, class LocMap>
-class distance_heuristic : public astar_heuristic<Graph, CostType>
-{
-public:
-	typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
-	distance_heuristic(LocMap l, Vertex goal)
-		: m_location(l), m_goal(goal) {}
-	CostType operator()(Vertex u)
-	{
-		CostType dx = m_location[m_goal].x - m_location[u].x;
-		CostType dy = m_location[m_goal].y - m_location[u].y;
-		return ::sqrt(dx * dx + dy * dy);
-	}
-private:
-	LocMap m_location;
-	Vertex m_goal;
-};
 
 
 
--- PathFinding1/Src/Abstraction/Heuristic.h
+++ PathFinding1/Src/Abstraction/Heuristic.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <boost/graph/astar_search.hpp>
+#include <math.h>    // for sqrt
+
+using namespace boost;
+using namespace std;
+
+// euclidean distance heuristic
+template <class Graph, class CostType, class LocMap>
+class distance_heuristic : public astar_heuristic<Graph, CostType>
+{
+public:
+	typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
+	distance_heuristic(LocMap l, Vertex goal)
+		: m_location(l), m_goal(goal) {}
+	CostType operator()(Vertex u)
+	{
+		CostType dx = m_location[m_goal].x - m_location[u].x;
+		CostType dy = m_location[m_goal].y - m_location[u].y;
+		return ::sqrt(dx * dx + dy * dy);
+	}
+private:
+	LocMap m_location;
+	Vertex m_goal;
+};
