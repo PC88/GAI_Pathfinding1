d59c83a48f328d64cc13ca2dc82e49cf0dea1d64
PC88 <32823980+PC88@users.noreply.github.com>
2020-02-21 05:02:41 +0000

    visitor refactoered to own .h

--- PathFinding1/Entrypoint.cpp
+++ PathFinding1/Entrypoint.cpp
@@ -62,22 +62,7 @@ private:
 };
 
 
-struct found_goal {}; // exception for termination
 
-// visitor that terminates when we find the goal
-template <class Vertex>
-class astar_goal_visitor : public boost::default_astar_visitor
-{
-public:
-	astar_goal_visitor(Vertex goal) : m_goal(goal) {}
-	template <class Graph>
-	void examine_vertex(Vertex u, Graph& g) {
-		if (u == m_goal)
-			throw found_goal();
-	}
-private:
-	Vertex m_goal;
-};
 
 
 int main(int argc, char **argv)
--- PathFinding1/Src/Abstraction/Visitor.h
+++ PathFinding1/Src/Abstraction/Visitor.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <boost/graph/astar_search.hpp>
+
+using namespace boost;
+using namespace std;
+
+struct found_goal {}; // exception for termination
+
+// visitor that terminates when we find the goal
+template <class Vertex>
+class astar_goal_visitor : public boost::default_astar_visitor
+{
+public:
+	astar_goal_visitor(Vertex goal) : m_goal(goal) {}
+	template <class Graph>
+	void examine_vertex(Vertex u, Graph& g) {
+		if (u == m_goal)
+			throw found_goal();
+	}
+private:
+	Vertex m_goal;
+};
